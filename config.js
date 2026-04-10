fcb3b753ceea8ff72ccffecd33177a46678fea29
RedMegaBoy <57818027+RedMegaBoy@users.noreply.github.com>
2026-04-11 01:34:55 +0300

    Update USERNAME and ASSETS_URL in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // Твій нікнейм для генерації голови та тексту
-    USERNAME: "RedMegaBoy",
+    USERNAME: "Hyuiawn",
     
     // Твоє посилання на таблицю (ВЖЕ ПІДКЛЮЧЕНО)
     CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTXH2rrMzPDAfkBKg29TMW0y211c8KBMrdjYLMo295unk4romp_f9NulXlAUTIRr9gOT5bw3CL_qtHt/pub?output=csv",
@@ -12,5 +12,5 @@ const CONFIG = {
     CURRENCY_RATE: 3, // 1 алмазна руда = 3 алмази
     
     // Базові посилання для іконок предметів (використовуємо надійний репозиторій)
-    ASSETS_URL: "https://raw.githubusercontent.com/PrismarineJS/minecraft-assets/master/data/1.20/items/"
+    ASSETS_URL: "https://github.com/PrismarineJS/minecraft-assets/tree/master/data/1.21.8/items"
 };
