c717a61df5916c16c18de19924e07e4a41ea7129
RedMegaBoy <57818027+RedMegaBoy@users.noreply.github.com>
2026-04-11 01:22:11 +0300

    Add configuration settings for the application

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    // Твій нікнейм для генерації голови та тексту
+    USERNAME: "RedMegaBoy",
+    
+    // Твоє посилання на таблицю (ВЖЕ ПІДКЛЮЧЕНО)
+    CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTXH2rrMzPDAfkBKg29TMW0y211c8KBMrdjYLMo295unk4romp_f9NulXlAUTIRr9gOT5bw3CL_qtHt/pub?output=csv",
+    
+    // Сюди вставиш посилання на Webhook з налаштувань каналу Discord
+    DISCORD_WEBHOOK_URL: "", 
+    
+    // Економіка
+    CURRENCY_RATE: 3, // 1 алмазна руда = 3 алмази
+    
+    // Базові посилання для іконок предметів (використовуємо надійний репозиторій)
+    ASSETS_URL: "https://raw.githubusercontent.com/PrismarineJS/minecraft-assets/master/data/1.20/items/"
+};
