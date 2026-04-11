86f8c60fdb6f4620ec15a3389e774eff2c4f0dc5
RedMegaBoy <57818027+RedMegaBoy@users.noreply.github.com>
2026-04-11 04:22:36 +0300

    Remove user configuration from config.js

--- config.js
+++ config.js
@@ -8,19 +8,4 @@ const CONFIG = {
     ASSETS_BLOCKS_URL: "https://raw.githubusercontent.com/PrismarineJS/minecraft-assets/master/data/1.21.8/blocks/",
     // Надійний сервіс для голів
     AVATAR_API: "https://mc-heads.net/avatar/"
-};const CONFIG = {
-    // Твій нікнейм для генерації голови та тексту
-    USERNAME: "Hyuiawn",
-    
-    // Твоє посилання на таблицю (ВЖЕ ПІДКЛЮЧЕНО)
-    CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTXH2rrMzPDAfkBKg29TMW0y211c8KBMrdjYLMo295unk4romp_f9NulXlAUTIRr9gOT5bw3CL_qtHt/pub?output=csv",
-    
-    // Сюди вставиш посилання на Webhook з налаштувань каналу Discord
-    DISCORD_WEBHOOK_URL: "", 
-    
-    // Економіка
-    CURRENCY_RATE: 3, // 1 алмазна руда = 3 алмази
-    
-    // Базові посилання для іконок предметів (використовуємо надійний репозиторій)
-    ASSETS_URL: "https://github.com/PrismarineJS/minecraft-assets/tree/master/data/1.21.8/items"
 };
