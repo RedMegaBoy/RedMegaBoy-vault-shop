cece451604e7158033e580ee4e2847dda90c38ac
RedMegaBoy <57818027+RedMegaBoy@users.noreply.github.com>
2026-04-11 05:06:03 +0300

    Change USERNAME from 'RedMegaBoy' to 'Hyuiawn'

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    USERNAME: "RedMegaBoy",
+    USERNAME: "Hyuiawn",
     CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTXH2rrMzPDAfkBKg29TMW0y211c8KBMrdjYLMo295unk4romp_f9NulXlAUTIRr9gOT5bw3CL_qtHt/pub?output=csv",
     DISCORD_WEBHOOK_URL: "", 
     CURRENCY_RATE: 3, // 1 руда = 3 алмази
